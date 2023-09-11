f07d6dd862507e883f857434d2cad70fa35f1b99
codecampos <devcodecampos@gmail.com>
2023-09-11 15:48:54 -0300

    feat: add pokemon model

---
+++ src/modules/Pokemon/Pokemon.model.ts
@@ -0,0 +1,13 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../../database";
+
+export const Pokemon = sequelize.define("Pokemon", {
+  pokedex_id: { type: DataTypes.SMALLINT, allowNull: false },
+  name: { type: DataTypes.STRING, allowNull: false },
+  height: { type: DataTypes.SMALLINT, allowNull: false },
+  weight: { type: DataTypes.SMALLINT, allowNull: false },
+  abilities: { type: DataTypes.ARRAY, allowNull: false },
+  image_url: { type: DataTypes.STRING, allowNull: false },
+  types: { type: DataTypes.ARRAY, allowNull: false },
+  species: { type: DataTypes.STRING, allowNull: false },
+});
