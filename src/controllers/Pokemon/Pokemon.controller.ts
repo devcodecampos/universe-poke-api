a19a74230d67033dace08f23f4f13972208632c4
codecampos <devcodecampos@gmail.com>
2023-09-11 17:12:28 -0300

    feat: add 'store' method to create a Pokémon

---
+++ src/controllers/Pokemon/Pokemon.controller.ts
@@ -0,0 +1,37 @@
+import { Pokemon } from "../../modules/Pokemon/Pokemon.model";
+import { Request, Response } from "express";
+
+class PokemonController {
+  async store(req: Request, res: Response) {
+    await Pokemon.sync();
+
+    const {
+      pokedex_id,
+      name,
+      height,
+      weight,
+      abilities,
+      image_url,
+      types,
+      species,
+    } = req.body;
+
+    try {
+      const pokemon = await Pokemon.create({
+        pokedex_id,
+        name,
+        height,
+        weight,
+        abilities,
+        image_url,
+        types,
+        species,
+      });
+
+      res.status(201).json(pokemon);
+    } catch (error) {
+      res.status(500).json({ error: "Error when creating a Pokémon" });
+    }
+  }
+}
+export default new PokemonController();
